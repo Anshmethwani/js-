0ae1ed140e12face67630b2b4cbfc60bbab63515
Anshmethwani <116326157+Anshmethwani@users.noreply.github.com>
2024-04-22 15:18:06 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version of js 
+// alert(3+3) // we're using nodejs not browser
+//tc39.es\ecma official documentation of js
+let name = "ansh"
+let age = 19
+let isLoggedIn = true
+
+//number
+//bigint
+//string
+//boolean
+//null => standalone value
+// undefined =>
+//symbol => unique
+
+//object
+console.log( typeof age);
+console.log(typeof null) // it is an object
+console.log(typeof undefined) // it is a type
